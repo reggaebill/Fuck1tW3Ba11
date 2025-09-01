91bcd566997b003ad6956e5ca44e6484863f1b95
reggaebill <aarongerhart7@gmail.com>
2025-08-31 23:26:04 -0400

    Add files via upload

---
+++ TheSummoning.py
@@ -0,0 +1,21 @@
+from cryptography.fernet import Fernet
+
+
+###you know what to do
+key = b''
+f = Fernet(key)
+
+###use this next line to encrypt a message. String must be prefixed with b' and suffixed with '
+#token = f.encrypt(b"")
+
+###use this line's output to update ReadME
+#print(token)
+
+
+###update token with new message to decrypt
+token = b''
+
+###this is the juice.
+message = (f.decrypt(token))
+
+print(message)
